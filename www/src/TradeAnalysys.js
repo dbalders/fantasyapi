b0b52d2c8fac7777bcf859e8cd8eeaad173361c2
David Balderston <dbalders@gmail.com>
2019-01-21 17:26:48 -0800

    move compare table into the trade page

--- www/src/TradeAnalysys.js
+++ www/src/TradeAnalysys.js
@@ -1,8 +1,11 @@
 import React, { Component } from 'react';
 import ReactTable from 'react-table';
 import 'react-table/react-table.css';
-import { CompareTeams } from './CompareTeams';
+import { TradeTeams } from './TradeTeams';
 import Cookies from 'js-cookie';
+import Select from 'react-select';
+import stringSimilarity from 'string-similarity';
+import { callApi } from './CallApi';
 
 export class TradeAnalysis extends Component {
     constructor(props) {
@@ -17,8 +20,14 @@ export class TradeAnalysis extends Component {
             teamStatsRecentAvg: [],
             playerRankingsSeason: [],
             playerRankingsRecent: [],
+            compareStatsSeason: [],
+            compareStatsRecent: [],
+            teamSelected: [],
+            teamPlayers: [],
+            showCompareTable: false,
             updateCompareTable: false
         }
+        this.hideCompareTable = this.hideCompareTable.bind(this);
     }
 
     componentDidMount() {
@@ -48,6 +57,110 @@ export class TradeAnalysis extends Component {
         }
     }
 
+    handleTeamChange = (teamSelected) => {
+        this.setState({ teamSelected });
+        callApi('/api/teams/' + this.state.leagueId + '/' + teamSelected.value)
+            .then(results => {
+                this.setState({showCompareTable: true});
+                this.setState({teamPlayers: results}, function() {
+                    this.buildTradeTeam(this.state.teamPlayers);
+                })
+            })
+            .catch(err => console.log(err));
+    }
+
+    //hide the compare table when clicked
+    hideCompareTable() {
+        this.setState({showCompareTable: false})
+    }
+
+    buildTradeTeam(team) {
+        var teamStatsSeason = [];
+        var teamStatsRecent = [];
+        var teamStatsSeasonAvg = [];
+        var teamStatsRecentAvg = [];
+        var teamPlayers = team;
+        var playerRankingsSeason = this.state.playerRankingsSeason;
+        var playerRankingsRecent = this.state.playerRankingsRecent;
+
+        //for each player on the team, if string similarity > .7 in the player rankings, then add that player to the array
+        for (var i = 0; i < teamPlayers.length; i++) {
+            for (var j = 0; j < playerRankingsSeason.length; j++) {
+                var similarPlayerSeason = stringSimilarity.compareTwoStrings(teamPlayers[i].full, playerRankingsSeason[j].playerName);
+                if (similarPlayerSeason > 0.7) {
+                    //Push the player to the team array
+                    teamStatsSeason.push(playerRankingsSeason[j]);
+                    //Start calculating averages by adding them all up
+                    teamStatsSeasonAvg = {
+                        ptsRating: (teamStatsSeasonAvg.ptsRating) ? (teamStatsSeasonAvg.ptsRating + playerRankingsSeason[j].ptsRating) : playerRankingsSeason[j].ptsRating,
+                        threeRating: (teamStatsSeasonAvg.threeRating) ? (teamStatsSeasonAvg.threeRating + playerRankingsSeason[j].threeRating) : playerRankingsSeason[j].threeRating,
+                        astRating: (teamStatsSeasonAvg.astRating) ? (teamStatsSeasonAvg.astRating + playerRankingsSeason[j].astRating) : playerRankingsSeason[j].astRating,
+                        rebRating: (teamStatsSeasonAvg.rebRating) ? (teamStatsSeasonAvg.rebRating + playerRankingsSeason[j].rebRating) : playerRankingsSeason[j].rebRating,
+                        stlRating: (teamStatsSeasonAvg.stlRating) ? (teamStatsSeasonAvg.stlRating + playerRankingsSeason[j].stlRating) : playerRankingsSeason[j].stlRating,
+                        blkRating: (teamStatsSeasonAvg.blkRating) ? (teamStatsSeasonAvg.blkRating + playerRankingsSeason[j].blkRating) : playerRankingsSeason[j].blkRating,
+                        fgMixedRating: (teamStatsSeasonAvg.fgMixedRating) ? (teamStatsSeasonAvg.fgMixedRating + playerRankingsSeason[j].fgMixedRating) : playerRankingsSeason[j].fgMixedRating,
+                        ftMixedRating: (teamStatsSeasonAvg.ftMixedRating) ? (teamStatsSeasonAvg.ftMixedRating + playerRankingsSeason[j].ftMixedRating) : playerRankingsSeason[j].ftMixedRating,
+                        toRating: (teamStatsSeasonAvg.toRating) ? (teamStatsSeasonAvg.toRating + playerRankingsSeason[j].toRating) : playerRankingsSeason[j].toRating
+                    }
+                    break
+                }
+            }
+
+            //Same here for recent data
+            for (var j = 0; j < playerRankingsRecent.length; j++) {
+                var similarPlayerRecent = stringSimilarity.compareTwoStrings(teamPlayers[i].full, playerRankingsRecent[j].playerName);
+                if (similarPlayerRecent > 0.7) {
+                    teamStatsRecent.push(playerRankingsRecent[j]);
+                    teamStatsRecentAvg = {
+                        ptsRating: (teamStatsRecentAvg.ptsRating) ? (teamStatsRecentAvg.ptsRating + playerRankingsRecent[j].ptsRating) : playerRankingsRecent[j].ptsRating,
+                        threeRating: (teamStatsRecentAvg.threeRating) ? (teamStatsRecentAvg.threeRating + playerRankingsRecent[j].threeRating) : playerRankingsRecent[j].threeRating,
+                        astRating: (teamStatsRecentAvg.astRating) ? (teamStatsRecentAvg.astRating + playerRankingsRecent[j].astRating) : playerRankingsRecent[j].astRating,
+                        rebRating: (teamStatsRecentAvg.rebRating) ? (teamStatsRecentAvg.rebRating + playerRankingsRecent[j].rebRating) : playerRankingsRecent[j].rebRating,
+                        stlRating: (teamStatsRecentAvg.stlRating) ? (teamStatsRecentAvg.stlRating + playerRankingsRecent[j].stlRating) : playerRankingsRecent[j].stlRating,
+                        blkRating: (teamStatsRecentAvg.blkRating) ? (teamStatsRecentAvg.blkRating + playerRankingsRecent[j].blkRating) : playerRankingsRecent[j].blkRating,
+                        fgMixedRating: (teamStatsRecentAvg.fgMixedRating) ? (teamStatsRecentAvg.fgMixedRating + playerRankingsRecent[j].fgMixedRating) : playerRankingsRecent[j].fgMixedRating,
+                        ftMixedRating: (teamStatsRecentAvg.ftMixedRating) ? (teamStatsRecentAvg.ftMixedRating + playerRankingsRecent[j].ftMixedRating) : playerRankingsRecent[j].ftMixedRating,
+                        toRating: (teamStatsRecentAvg.toRating) ? (teamStatsRecentAvg.toRating + playerRankingsRecent[j].toRating) : playerRankingsRecent[j].toRating
+                    }
+                    break
+                }
+            }
+        }
+        
+        this.setState({ compareStatsSeason: teamStatsSeason });
+        this.setState({ compareStatsRecent: teamStatsRecent });
+
+        //Divide averages total by the number of players they have to get avg number
+        teamStatsSeasonAvg = {
+            ptsRating: Number(teamStatsSeasonAvg.ptsRating / teamStatsSeason.length).toFixed(2),
+            threeRating: Number(teamStatsSeasonAvg.threeRating / teamStatsSeason.length).toFixed(2),
+            astRating: Number(teamStatsSeasonAvg.astRating / teamStatsSeason.length).toFixed(2),
+            rebRating: Number(teamStatsSeasonAvg.rebRating / teamStatsSeason.length).toFixed(2),
+            stlRating: Number(teamStatsSeasonAvg.stlRating / teamStatsSeason.length).toFixed(2),
+            blkRating: Number(teamStatsSeasonAvg.blkRating / teamStatsSeason.length).toFixed(2),
+            fgMixedRating: Number(teamStatsSeasonAvg.fgMixedRating / teamStatsSeason.length).toFixed(2),
+            ftMixedRating: Number(teamStatsSeasonAvg.ftMixedRating / teamStatsSeason.length).toFixed(2),
+            toRating: Number(teamStatsSeasonAvg.ftRating / teamStatsSeason.length).toFixed(2)
+        }
+
+
+        teamStatsRecentAvg = {
+            ptsRating: Number(teamStatsRecentAvg.ptsRating / teamStatsRecent.length).toFixed(2),
+            threeRating: Number(teamStatsRecentAvg.threeRating / teamStatsRecent.length).toFixed(2),
+            astRating: Number(teamStatsRecentAvg.astRating / teamStatsRecent.length).toFixed(2),
+            rebRating: Number(teamStatsRecentAvg.rebRating / teamStatsRecent.length).toFixed(2),
+            stlRating: Number(teamStatsRecentAvg.stlRating / teamStatsRecent.length).toFixed(2),
+            blkRating: Number(teamStatsRecentAvg.blkRating / teamStatsRecent.length).toFixed(2),
+            fgMixedRating: Number(teamStatsRecentAvg.fgMixedRating / teamStatsRecent.length).toFixed(2),
+            ftMixedRating: Number(teamStatsRecentAvg.ftMixedRating / teamStatsRecent.length).toFixed(2),
+            toRating: Number(teamStatsRecentAvg.ftRating / teamStatsRecent.length).toFixed(2)
+        }
+
+        //Put the [] around the arrays so the table below can know its a single row
+        this.setState({ compareStatsSeasonAvg: [teamStatsSeasonAvg] });
+        this.setState({ compareStatsRecentAvg: [teamStatsRecentAvg] });
+    }
+
     render() {
         var nameHeader = 'playerName';
         var rankHeader = 'overallRank';
@@ -67,6 +180,20 @@ export class TradeAnalysis extends Component {
         const lightRed = '#ffdfdf';
         const mediumRed = '#ffb8b8';
         const brightRed = '#ff8282';
+
+        const { teamSelected } = this.state;
+        const teamSelect = [];
+
+        //Get the team names from a prop and push it into teamSelect for select dropdown
+        for (var i = 0; i < this.state.teams.length; i++) {
+            teamSelect.push({ value: this.state.teams[i].team_id, label: this.state.teams[i].name })
+        }
+
+        //If the parent says to update the table, rebuild the table with new data
+        if ((this.state.updateCompareTable)) {
+            this.buildTradeTeam(this.state.teamPlayers);
+        }
+
 
         const columnNames = [{
             Header: 'Rank',
@@ -430,9 +557,9 @@ export class TradeAnalysis extends Component {
             className: "center"
         }]
 
-        var compareTeamsHTML = "";
+        var tradeTeamsHTML = "";
         if (this.state.leagueId) {
-            compareTeamsHTML = <CompareTeams leagueId={this.state.leagueId} teams={this.state.teams} columnNames={columnNames}
+            tradeTeamsHTML = <TradeTeams leagueId={this.state.leagueId} teams={this.state.teams} columnNames={columnNames}
                 playerRankingsSeason={this.state.playerRankingsSeason} playerRankingsRecent={this.state.playerRankingsRecent}
                 columnNamesAvg={columnNamesAvg} updateCompareTable={this.state.updateCompareTable} expandedColumnNames={expandedColumnNames} 
                 title="Team to trade with" />
@@ -476,7 +603,56 @@ export class TradeAnalysis extends Component {
                     />
                 </div>
 
-                {compareTeamsHTML}
+                <div className={`table-group ${this.state.showCompareTable ? 'compare-table-group' : ''}`}>
+                
+                <h3 className="team-table-header compare-header">Trade with team</h3>
+                <div className="flex">
+                    <div className="team-select">
+                        <Select
+                            value={teamSelected}
+                            onChange={this.handleTeamChange}
+                            options={teamSelect}
+                        />
+                    </div>
+                    <div className={`hide-button team-select ${this.state.showCompareTable ? '' : 'hide'}`} onClick = {this.hideCompareTable}>
+                        Hide Comparison
+                    </div>
+                </div>
+                <div className={`team-table ${this.state.showCompareTable ? '' : 'hide'}`}>
+                    {/* <div className="team-avg-table">
+                      <ReactTable
+                        data={this.state.compareStatsSeasonAvg}
+                        columns={this.props.columnNamesAvg}
+                        showPagination={false}
+                        minRows={0}
+                      />
+                    </div> */}
+                    <div className="team-table">
+                      <ReactTable
+                        data={this.state.compareStatsSeason}
+                        columns={columnNames}
+                        showPagination={false}
+                        minRows={0}
+                        defaultSortDesc={true}
+                        defaultSorted={[{
+                            id: 'overallRank',
+                            desc: false
+                        }]}
+                        SubComponent={row => {
+                            return (
+                                <ReactTable
+                                    data={[row.original]}
+                                    columns={expandedColumnNames}
+                                    showPagination={false}
+                                    defaultPageSize={1}
+                                    className="expandedRow"
+                                />
+                            );
+                        }}
+                      />
+                    </div>
+                </div>
+            </div>
 
             </div>
         )
